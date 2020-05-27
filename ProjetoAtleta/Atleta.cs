4062b3acba77cf7a4914cd7a8e3314b8af0e04d7
DD-jazer <65659403+DD-jazer@users.noreply.github.com>
2020-05-26 21:40:44 -0300

    Add files via upload

---
+++ ProjetoAtleta/Atleta.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAtleta
+{
+    class Atleta
+    {
+        private string nome;
+        private double peso, altura;
+        private int idade;
+
+        public string Nome
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.nome = value;
+                }
+                else
+                {
+                    throw new Exception("o nome deve ser devidamente preenchido.");
+                }
+            }
+            get { return this.nome; }
+        }
+        public double Peso
+        {
+            set
+            {
+                if (value >= 0.00)
+                {
+                    this.peso = value;
+                }
+                else
+                {
+                    throw new Exception("o peso deve ser positivo.");
+                }
+            }
+            get { return this.peso; }
+        }
+        public double Altura
+        {
+            set
+            {
+                if (value >= 0.00)
+                {
+                    this.altura = value;
+                }
+                else
+                {
+                    throw new Exception("a altura deve ser positiva.");
+                }
+            }
+            get { return this.altura; }
+        }
+        public int Idade
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    this.idade = value;
+                }
+                else
+                {
+                    throw new Exception("A idade deve ser inteira e positiva.");
+                }
+            }
+            get { return this.idade; }
+        }
+        public string ImprimeDados()
+        {
+            return "Seus Dados: \n\nNome: " + this.nome + 
+                   "\nIdade:" + this.idade;
+        }
+        public double CalcIMC()
+        {
+            return this.peso / (this.altura * this.altura);
+        }
+    }
+}
---
+++ ProjetoAtleta/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ProjetoAtleta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.lbl_altura = new System.Windows.Forms.Label();
+            this.lbl_peso = new System.Windows.Forms.Label();
+            this.lbl_idade = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.btn_enviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(12, 161);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(230, 23);
+            this.btn_calcular.TabIndex = 0;
+            this.btn_calcular.Text = "calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(12, 12);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(33, 13);
+            this.lbl_nome.TabIndex = 1;
+            this.lbl_nome.Text = "nome";
+            // 
+            // lbl_altura
+            // 
+            this.lbl_altura.AutoSize = true;
+            this.lbl_altura.Location = new System.Drawing.Point(139, 12);
+            this.lbl_altura.Name = "lbl_altura";
+            this.lbl_altura.Size = new System.Drawing.Size(33, 13);
+            this.lbl_altura.TabIndex = 2;
+            this.lbl_altura.Text = "altura";
+            // 
+            // lbl_peso
+            // 
+            this.lbl_peso.AutoSize = true;
+            this.lbl_peso.Location = new System.Drawing.Point(12, 67);
+            this.lbl_peso.Name = "lbl_peso";
+            this.lbl_peso.Size = new System.Drawing.Size(30, 13);
+            this.lbl_peso.TabIndex = 3;
+            this.lbl_peso.Text = "peso";
+            // 
+            // lbl_idade
+            // 
+            this.lbl_idade.AutoSize = true;
+            this.lbl_idade.Location = new System.Drawing.Point(139, 67);
+            this.lbl_idade.Name = "lbl_idade";
+            this.lbl_idade.Size = new System.Drawing.Size(33, 13);
+            this.lbl_idade.TabIndex = 4;
+            this.lbl_idade.Text = "idade";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(12, 28);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 5;
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(142, 28);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 6;
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(12, 84);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 20);
+            this.txt_peso.TabIndex = 7;
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(142, 84);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(100, 20);
+            this.txt_idade.TabIndex = 8;
+            // 
+            // btn_enviar
+            // 
+            this.btn_enviar.Location = new System.Drawing.Point(12, 120);
+            this.btn_enviar.Name = "btn_enviar";
+            this.btn_enviar.Size = new System.Drawing.Size(230, 23);
+            this.btn_enviar.TabIndex = 10;
+            this.btn_enviar.Text = "enviar dados";
+            this.btn_enviar.UseVisualStyleBackColor = true;
+            this.btn_enviar.Click += new System.EventHandler(this.btn_enviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 197);
+            this.Controls.Add(this.btn_enviar);
+            this.Controls.Add(this.txt_idade);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.lbl_idade);
+            this.Controls.Add(this.lbl_peso);
+            this.Controls.Add(this.lbl_altura);
+            this.Controls.Add(this.lbl_nome);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Label lbl_altura;
+        private System.Windows.Forms.Label lbl_peso;
+        private System.Windows.Forms.Label lbl_idade;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.TextBox txt_idade;
+        private System.Windows.Forms.Button btn_enviar;
+    }
+}
+
